cc56c7f50a9b7271f73a873a737e007fb859fe0b
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-31 12:39:31 +0300

    init alert project

---
+++ Basic Alert/Basic Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Alert
+//
+//  Created by cemal tüysüz on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

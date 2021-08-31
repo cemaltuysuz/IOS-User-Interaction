22f774cfa584748b6cbd0f58034a8d8f8cb5f2dc
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-31 13:20:02 +0300

    private alert

--- Basic Alert/Basic Alert/ViewController.swift
+++ Basic Alert/Basic Alert/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func BasicAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Title", message: "your message", preferredStyle: .alert)
+        
+        // Dismiss Action
+        let dismiss = UIAlertAction(title: "Dismiss", style: .cancel){
+            action in
+            self.labelResult.text = "Basic alert dismiss"}
+        
+        alert.addAction(dismiss) // Dismiss action inserted to alert
+        
+        // Ok Action
+        let ok = UIAlertAction(title: "Okey", style: .default){
+            action in
+            self.labelResult.text = "Basic alert ok"}
+        
+        alert.addAction(ok) // Ok Action inserted to alert
+        
+        self.present(alert,animated: true)
+    }
+    
+    
+    @IBAction func privateAlert(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Title", message: "your message", preferredStyle: .alert)
+        
+        alert.addTextField{ textfield in
+            textfield.placeholder = "e-mail"
+            textfield.keyboardType = .emailAddress
+        }
+        alert.addTextField{ textfield in
+            textfield.placeholder = "password"
+            textfield.isSecureTextEntry = true
+            
+        }
+        
+        // Dismiss Action
+        let save = UIAlertAction(title: "Save", style: .cancel){
+            action in
+            let mail = (alert.textFields![0] as UITextField).text
+            let password = (alert.textFields![1] as UITextField).text
+            self.labelResult.text = "Mail : \(mail!) pass : \(password!)"
+            
+        }
+        
+        alert.addAction(save) // Dismiss action inserted to alert
+        
+        self.present(alert,animated: true)
+    }
+    
+    @IBAction func ActionSheet(_ sender: Any) {
+    }
 }
 
